46f8f7f6f8ae6884cd3b0fd09adf9361aa83253d
sharik10ahmed <sharikahmed757@gmail.com>
2026-06-11 15:43:47 +0530

    added

---
+++ Day 3/Odd_check.java
@@ -0,0 +1,11 @@
+public class Odd_check{
+    public static void main(String[] args){
+        int num = 19;
+        if (num%2==0){
+            System.out.println("Even");
+        }
+        else{
+            System.out.println("Odd");
+        }
+    }
+}
