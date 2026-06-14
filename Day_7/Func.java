251553e3547121d057d6aebcaa34d33f4f2566a7
sharik10ahmed <sharikahmed757@gmail.com>
2026-06-14 13:02:14 +0530

    added

---
+++ Day_7/Func.java
@@ -0,0 +1,11 @@
+package Day_7;
+
+public class Func{
+    public static void sum(int a,int b){
+    int sum = a+b;
+        System.out.println(sum);
+    }
+    public static void main(String[] args){
+        sum(5,2);
+    }
+}
---
+++ Day_7/mul_func.java
@@ -0,0 +1,11 @@
+package Day_7;
+
+public class mul_func{
+    public static void product(int a,int b){
+        int product = a*b;
+        System.out.println(product);
+    }
+    public static void main(String[] args){
+        product(5,2);
+    }
+}
