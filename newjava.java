20868b15785c1507bfaf4e91c0d57d528f61fd73
sharik10ahmed <sharikahmed757@gmail.com>
2026-06-15 12:23:49 +0530

    newfiles

---
+++ newjava.java
@@ -0,0 +1,19 @@
+public class newjava {
+    // Recursive method to print numbers descending
+    public static void printCountdown(int n) {
+        // Base case: stop when n hits 0
+        if (n <= 0) {
+            return;
+        }
+
+        System.out.println(n);
+
+        // Recursive call with a smaller sub-problem
+        printCountdown(n - 1);
+    }
+
+    public static void main(String[] args) {
+        // Starts the recursion from 5
+        printCountdown(5);
+    }
+}
